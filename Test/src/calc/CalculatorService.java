4deea44b002b1c05b30eb9ad5747820b94b6f6fe
dir-FY24yasudayuzuha <yuzuha.yasuda.f566@dirbato.co.jp>
2024-04-10 10:43:10 +0900

    テスト（JUnit）の練習です。

---
+++ Test/src/calc/CalculatorService.java
@@ -0,0 +1,21 @@
+package calc;
+
+public class CalculatorService {
+    private Calculator calculator;
+    
+    public CalculatorService(Calculator calculator) {
+        this.calculator = calculator;
+    }
+    
+    public String addAndReport(int a, int b) {
+        int result = calculator.add(a, b);
+        return "Result: " + result;
+    }
+    
+    public String subtractAndReport(int a, int b) {
+        int result = calculator.subtract(a, b);
+        return "Result: " + result;
+        
+    }
+
+}
